10b35c928212181aa8119caa0ef871bf5042511b
Mantasblo <mantax559@gmail.com>
2024-02-21 19:48:22 +0200

    Code cleaning

--- src/Providers/AppServiceProvider.php
+++ src/Providers/AppServiceProvider.php
@@ -2,30 +2,29 @@
 
 namespace Mantax559\LaravelSettings\Providers;
 
-use App\Providers\EventServiceProvider;
 use Illuminate\Support\ServiceProvider;
 use Mantax559\LaravelSettings\Models\Setting;
 use Mantax559\LaravelSettings\Observers\SettingObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
-    private const CONFIG_FILE = __DIR__.'/../../config/laravel-settings.php';
+    private const PATH_CONFIG = __DIR__.'/../../config/laravel-settings.php';
 
-    private const MIGRATION_FILES = __DIR__.'/../../database/migrations';
+    private const PATH_MIGRATIONS = __DIR__.'/../../database/migrations';
 
     public function boot(): void
     {
         $this->publishes([
-            self::CONFIG_FILE => config_path('laravel-settings.php'),
+            self::PATH_CONFIG => config_path('laravel-settings.php'),
         ], 'config');
 
-        $this->loadMigrationsFrom(self::MIGRATION_FILES);
+        $this->loadMigrationsFrom(self::PATH_MIGRATIONS);
 
         Setting::observe(SettingObserver::class);
     }
 
     public function register(): void
     {
-        $this->mergeConfigFrom(self::CONFIG_FILE, 'laravel-settings');
+        $this->mergeConfigFrom(self::PATH_CONFIG, 'laravel-settings');
     }
 }
